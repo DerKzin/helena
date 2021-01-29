cb656e939228f8ffc1a98dae00c518112c48c29f
DerKzin <marcosatony44@gmail.com>
2021-01-29 14:25:24 -0300

    First commit

---
+++ src/help.js
@@ -0,0 +1,213 @@
+const help = (prefix) => {
+	return `                 
+┏━━°❀ ❬ *🌸HELENA BOT🌸* ❭ ❀°━━┓
+┃
+┣➥ *${prefix}info*
+┣➥ *${prefix}dono*
+┣➥ *${prefix}speed*
+┣➥ *${prefix}report [lapor bug]*
+┃
+┣━━━━°❀ ❬ *MEDIA* ❭ ❀°━━━━━⊱
+┃
+┣➥ *${prefix}stiker*
+┣➥ *${prefix}tsticker*
+┣➥ *${prefix}toimg*
+┣➥ *${prefix}nulis*
+┣➥ *${prefix}ocr*
+┣➥ *${prefix}ytsearch*
+┣➥ *${prefix}ytmp3*
+┣➥ *${prefix}ytmp4*
+┣➥ *${prefix}tiktok*
+┣➥ *${prefix}tiktokstalk*
+┣➥ *${prefix}fototiktok*
+┣➥ *${prefix}igstalk*
+┣➥ *${prefix}image*
+┣➥ *${prefix}pinterest*
+┣➥ *${prefix}gtts*
+┣➥ *${prefix}meme*
+┣➥ *${prefix}memeindo*
+┣➥ *${prefix}ssweb*
+┣➥ *${prefix}walpaperhd*
+┣➥ *${prefix}randomcat*
+┣➥ *${prefix}joox*
+┣➥ *${prefix}inu*
+┣➥ *${prefix}elang*
+┣➥ *${prefix}unta*
+┣➥ *${prefix}anjing*
+┣➥ *${prefix}babi*
+┣➥ *${prefix}playstore*
+┣➥ *${prefix}url2image*
+┣➥ *${prefix}kbbi*
+┣➥ *${prefix}imoji*
+┣➥ *${prefix}wait*
+┃
+┣━━━━°❀ ❬ *CREATOR* ❭ ❀°━━━━⊱
+┃
+┣➥ *${prefix}thunder*
+┣➥ *${prefix}tahta*
+┣➥ *${prefix}glitch <teks|teks>*
+┣➥ *${prefix}phlogo <teks|teks>*
+┣➥ *${prefix}wolflogo <teks|teks>*
+┣➥ *${prefix}wolflogo2 <teks|teks>*
+┣➥ *${prefix}quotemaker <tx|wtrmk|tema>*
+┣➥ *${prefix}galaxtext*
+┣➥ *${prefix}textdark*
+┣➥ *${prefix}textblue*
+┣➥ *${prefix}lovemake*
+┣➥ *${prefix}stiltext*
+┣➥ *${prefix}ninjalogo*
+┣➥ *${prefix}party*
+┣➥ *${prefix}rtext*
+┣➥ *${prefix}water*
+┣➥ *${prefix}lionlogo <teks|teks>*
+┣➥ *${prefix}textscreen*
+┣➥ *${prefix}text3d*
+┣➥ *${prefix}epep*
+┣➥ *${prefix}marvelogo <teks|teks>*
+┣➥ *${prefix}snow <teks|teks>*
+┣➥ *${prefix}firetext*
+┃
+┣━━━°❀ ❬ *FUN&GAMES* ❭ ❀°━━━⊱
+┃
+┣➥ *${prefix}truth*
+┣➥ *${prefix}dare*
+┣➥ *${prefix}tebakgambar*
+┣➥ *${prefix}family100*
+┣➥ *${prefix}caklontong*
+┣➥ *${prefix}game*
+┣➥ *${prefix}primbonjodoh*
+┣➥ *${prefix}ramaljadian*
+┣➥ *${prefix}mlherolist*
+┣➥ *${prefix}bucin*
+┣➥ *${prefix}persengay*
+┣➥ *${prefix}ramalhp <nomor>*
+┣➥ *${prefix}ceckjodoh*
+┃
+┣━━━━━°❀ ❬ *ANIMES* ❭ ❀°━━━━━⊱
+┃
+┣➥ *${prefix}openanime*
+┣➥ *${prefix}Naruto*
+┣➥ *${prefix}Minato*
+┣➥ *${prefix}Boruto*
+┣➥ *${prefix}Hinata*
+┣➥ *${prefix}Sakura*
+┣➥ *${prefix}Sasuke*
+┣➥ *${prefix}kaneki*
+┣➥ *${prefix}toukacan*
+┣➥ *${prefix}rize*
+┣➥ *${prefix}akira*
+┣➥ *${prefix}itori*
+┣➥ *${prefix}kurumi*
+┣➥ *${prefix}miku*
+┣➥ *${prefix}anime*
+┣➥ *${prefix}nekonime*
+┣➥ *${prefix}loli*
+┣➥ *${prefix}loli2*
+┣➥ *${prefix}waifu*
+┣➥ *${prefix}waifu2*
+┣➥ *${prefix}wibu*
+┣➥ *${prefix}randomanime*
+┣➥ *${prefix}pokemon*
+┣➥ *${prefix}artinama*
+┃
+┣━━°❀ ❬ *INFO&EDUCAÇÃO* ❭ ❀°━━⊱
+┃
+┣➥ *${prefix}infogc*
+┣➥ *${prefix}infogempa*
+┣➥ *${prefix}infogithub*
+┣➥ *${prefix}infocuaca*
+┣➥ *${prefix}infonomor*
+┣➥ *${prefix}infomobil*
+┣➥ *${prefix}infomotor*
+┣➥ *${prefix}grupinfo*
+┣➥ *${prefix}lirik*
+┣➥ *${prefix}quotes*
+┣➥ *${prefix}cerpen*
+┣➥ *${prefix}chord*
+┣➥ *${prefix}wiki*
+┣➥ *${prefix}brainly*
+┣➥ *${prefix}resepmasakan*
+┣➥ *${prefix}map*
+┃
+┣━━━━━°❀ ❬ *GRUPO* ❭ ❀°━━━━━⊱
+┃
+┣➥ *${prefix}add*
+┣➥ *${prefix}kick*
+┣➥ *${prefix}promote*
+┣➥ *${prefix}demote*
+┣➥ *${prefix}setname*
+┣➥ *${prefix}setdesc*
+┣➥ *${prefix}welcome*
+┣➥ *${prefix}nsfw*
+┣➥ *${prefix}simih*
+┣➥ *${prefix}grup [abrir/fechar]*
+┣➥ *${prefix}tagme*
+┣➥ *${prefix}hidetag*
+┣➥ *${prefix}tagall*
+┣➥ *${prefix}otagall*
+┣➥ *${prefix}fitnah*
+┣➥ *${prefix}infogc*
+┣➥ *${prefix}grupinfo*
+┣➥ *${prefix}linkgrup*
+┣➥ *${prefix}listadmins*
+┣➥ *${prefix}openanime*
+┣➥ *${prefix}edotense*
+┣➥ *${prefix}kudeta*
+┃
+┣━━━━━°❀ ❬ *NSFW* ❭ ❀°━━━━━⊱
+┃
+┣➥ *${prefix}nsfwloli*
+┣➥ *${prefix}loli1
+┣➥ *${prefix}loli2
+┣➥ *${prefix}nsfwtrap*
+┣➥ *${prefix}hentai*
+┣➥ *${prefix}porno*
+┣➥ *${prefix}belle*
+┣➥ *${prefix}belle2*
+┣➥ *${prefix}belle3
+┣➥ *${prefix}mia*
+┃
+┣━━━━°❀ ❬ *CARTUCHOS* ❭ ❀°━━━━━⊱
+┃
+┣➥ *${prefix}apakah*
+┣➥ *${prefix}kapankah*
+┣➥ *${prefix}bisakah*
+┣➥ *${prefix}rate*
+┣➥ *${prefix}watak*
+┣➥ *${prefix}hobby*
+┃
+┣━━━━━°❀ ❬ *OUTROS* ❭ ❀°━━━━━⊱
+┃
+┣➥ *${prefix}blocklist*
+┣➥ *${prefix}testime*
+┣➥ *${prefix}hilih*
+┣➥ *${prefix}say*
+┣➥ *${prefix}delete*
+┣➥ *${prefix}shorturl*
+┃
+┣━━━━°❀ ❬ *OWNER* ❭ ❀°━━━━━⊱
+┃
+┣➥ *${prefix}bc*
+┣➥ *${prefix}ban*
+┣➥ *${prefix}block*
+┣➥ *${prefix}unblock*
+┣➥ *${prefix}clearall*
+┣➥ *${prefix}clone*
+┣➥ *${prefix}getses*
+┣➥ *${prefix}setpp*
+┣➥ *${prefix}setpp*
+┣➥ *${prefix}leave*
+┃
+┃
+┣━━━━━━━━━━━━━━━━━━━━
+┃ *${prefix}POWERED BY EREN*
+┗━━━━━━━━━━━━━━━━━━━━`
+}
+
+exports.help = help
+
+
+
+
+
+
